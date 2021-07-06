63bb519fb56779ff3734deb2bea42f36ac7090f3
tooMofuu0216 <86454385+tooMofuu0216@users.noreply.github.com>
2021-07-06 14:57:15 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='image_sound_steganograph_test_module',
+    version='0.0.1',
+    packages=[''],
+    url='https://github.com/tooMofuu0216/Basic-LSB-Steganograph-Project',
+    license='',
+    author='tooMofuu0216',
+    author_email='bubi9101@gmail.com',
+    description='Basic-LSB-Steganograph-Project'
+)
